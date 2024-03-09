50b8691525be5e370994449f0d3eba698512bc8e
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 16:11:14 +0300

    Remove unused variable streamChan from StartUserDataStream function

--- streams/userdata.go
+++ streams/userdata.go
@@ -6,12 +6,10 @@ import (
 	"github.com/adshao/go-binance/v2"
 )
 
-func StartUserDataStream(listenKey string, wsHandler binance.WsUserDataHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error, streamChan chan binance.WsUserDataEvent) {
-	streamChan = make(chan binance.WsUserDataEvent)
+func StartUserDataStream(listenKey string, wsHandler binance.WsUserDataHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	doneC, stopC, err = binance.WsUserDataServe(listenKey, wsHandler, handleErr)
 	if err != nil {
 		log.Fatalf("Error serving user data websocket: %v", err)
-		streamChan = nil
 		return
 	}
 	return
