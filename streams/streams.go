4107678c9af2d267ce7fed02a647f32766b5058c
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 23:22:35 +0300

    Add stream functions for Kline, Trade, AggTrade, and BookTicker

--- streams/streams.go
+++ streams/streams.go
@@ -11,3 +11,19 @@ func StartUserDataStream(listenKey string, wsHandler binance.WsUserDataHandler, 
 func StartDepthStream(symbol string, wsHandler binance.WsDepthHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	return binance.WsDepthServe(symbol, wsHandler, handleErr)
 }
+
+func StartKlineStream(symbol string, interval string, wsHandler binance.WsKlineHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+	return binance.WsKlineServe(symbol, interval, wsHandler, handleErr)
+}
+
+func StartTradeStream(symbol string, wsHandler binance.WsTradeHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+	return binance.WsTradeServe(symbol, wsHandler, handleErr)
+}
+
+func StartAggTradeStream(symbol string, wsHandler binance.WsAggTradeHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+	return binance.WsAggTradeServe(symbol, wsHandler, handleErr)
+}
+
+func StartBookTickerStream(symbol string, wsHandler binance.WsBookTickerHandler, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+	return binance.WsBookTickerServe(symbol, wsHandler, handleErr)
+}
