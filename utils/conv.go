7e5fb80f875b248aa8d77a1b9e254ac1c4f50078
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 20:45:15 +0300

    Add ConvFloat64ToStrDefault function

--- utils/conv.go
+++ utils/conv.go
@@ -13,6 +13,10 @@ func ConvStrToFloat64(s string) float64 {
 	return f
 }
 
+func ConvFloat64ToStrDefault(f float64) string {
+	return strconv.FormatFloat(f, 'f', 8, 64)
+}
+
 func ConvFloat64ToStr(f float64, prec int) string {
 	return strconv.FormatFloat(f, 'f', prec, 64)
 }
