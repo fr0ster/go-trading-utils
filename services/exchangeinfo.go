fa58559cf4bee3fcc4ac198dd9e007c5a64d5bdd
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 17:56:49 +0300

    Add functions to retrieve order types and permissions for a specific symbol

--- services/exchangeinfo.go
+++ services/exchangeinfo.go
@@ -13,6 +13,27 @@ func GetExchangeInfo(client *binance.Client) (*binance.ExchangeInfo, error) {
 	return exchangeInfo, err
 }
 
+func GetOrderTypes(exchangeInfo *binance.ExchangeInfo, symbolname string) []binance.OrderType {
+	res := make([]binance.OrderType, 0)
+	for _, info := range exchangeInfo.Symbols {
+		if info.Symbol == symbolname {
+			for _, orderType := range info.OrderTypes {
+				res = append(res, binance.OrderType(orderType))
+			}
+		}
+	}
+	return res
+}
+
+func GetPermissions(exchangeInfo *binance.ExchangeInfo, symbolname string) []string {
+	for _, info := range exchangeInfo.Symbols {
+		if info.Symbol == symbolname {
+			return info.Permissions
+		}
+	}
+	return nil
+}
+
 func PrintFiltersInfo(client *binance.Client, symbolname string) {
 	exchangeInfo, err := client.NewExchangeInfoService().Do(context.Background())
 	if err != nil {
@@ -23,9 +44,7 @@ func PrintFiltersInfo(client *binance.Client, symbolname string) {
 		if info.Symbol == symbolname {
 			for _, filters := range info.Filters {
 				for _, filter := range filters {
-					if filter == "PRICE_FILTER" {
-						fmt.Printf("Filter: %s\n", filter)
-					}
+					fmt.Printf("Filter: %s\n", filter)
 				}
 			}
 		}
