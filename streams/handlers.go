6cf69c38a9137bd85e9f59abc13d90b84b8be54b
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 18:02:31 +0300

    Refactor GetFilledOrderHandler signature

--- streams/handlers.go
+++ streams/handlers.go
@@ -2,7 +2,7 @@ package streams
 
 import "github.com/adshao/go-binance/v2"
 
-func GetFilledOrderHandler(event *binance.WsUserDataEvent, executeOrderChan chan *binance.WsUserDataEvent) func(event *binance.WsUserDataEvent) {
+func GetFilledOrderHandler(executeOrderChan chan *binance.WsUserDataEvent) func(event *binance.WsUserDataEvent) {
 	return func(event *binance.WsUserDataEvent) {
 		if event.Event == binance.UserDataEventTypeExecutionReport && event.OrderUpdate.Status == string(binance.OrderStatusTypeFilled) {
 			executeOrderChan <- event
