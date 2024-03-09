de6d648608be63ad053614026b33781392cf967f
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 17:35:59 +0300

    Add functions to get exchange info and print filters info

--- services/exchangeinfo.go
+++ services/exchangeinfo.go
@@ -1,1 +1,33 @@
 package services
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/adshao/go-binance/v2"
+)
+
+func GetExchangeInfo(client *binance.Client) (*binance.ExchangeInfo, error) {
+	exchangeInfo, err := client.NewExchangeInfoService().Do(context.Background())
+	return exchangeInfo, err
+}
+
+func PrintFiltersInfo(client *binance.Client, symbolname string) {
+	exchangeInfo, err := client.NewExchangeInfoService().Do(context.Background())
+	if err != nil {
+		log.Fatalf("Error fetching exchange info: %v", err)
+	}
+
+	for _, info := range exchangeInfo.Symbols {
+		if info.Symbol == symbolname {
+			for _, filters := range info.Filters {
+				for _, filter := range filters {
+					if filter == "PRICE_FILTER" {
+						fmt.Printf("Filter: %s\n", filter)
+					}
+				}
+			}
+		}
+	}
+}
