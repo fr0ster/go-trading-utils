e606048b07c052692772619326ed21182296ef06
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-09 18:05:15 +0300

    Refactor GetFilledOrderHandler to return a channel for executing orders

--- streams/handlers.go
+++ streams/handlers.go
@@ -2,10 +2,11 @@ package streams
 
 import "github.com/adshao/go-binance/v2"
 
-func GetFilledOrderHandler(executeOrderChan chan *binance.WsUserDataEvent) func(event *binance.WsUserDataEvent) {
+func GetFilledOrderHandler() (func(event *binance.WsUserDataEvent), chan *binance.WsUserDataEvent) {
+	executeOrderChan := make(chan *binance.WsUserDataEvent, 1)
 	return func(event *binance.WsUserDataEvent) {
 		if event.Event == binance.UserDataEventTypeExecutionReport && event.OrderUpdate.Status == string(binance.OrderStatusTypeFilled) {
 			executeOrderChan <- event
 		}
-	}
+	}, executeOrderChan
 }
