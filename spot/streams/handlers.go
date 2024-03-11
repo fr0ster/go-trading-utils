769945cdfd8f4ec138f01f94ae9fb242bc8e2077
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-11 20:14:19 +0300

    Refactor GetFilledOrderHandler to include partially filled orders

--- spot/streams/handlers.go
+++ spot/streams/handlers.go
@@ -5,7 +5,9 @@ import "github.com/adshao/go-binance/v2"
 func GetFilledOrderHandler() (func(event *binance.WsUserDataEvent), chan *binance.WsUserDataEvent) {
 	executeOrderChan := make(chan *binance.WsUserDataEvent, 1)
 	return func(event *binance.WsUserDataEvent) {
-		if event.Event == binance.UserDataEventTypeExecutionReport && event.OrderUpdate.Status == string(binance.OrderStatusTypeFilled) {
+		if event.Event == binance.UserDataEventTypeExecutionReport &&
+			(event.OrderUpdate.Status == string(binance.OrderStatusTypeFilled) ||
+				event.OrderUpdate.Status == string(binance.OrderStatusTypePartiallyFilled)) {
 			executeOrderChan <- event
 		}
 	}, executeOrderChan
