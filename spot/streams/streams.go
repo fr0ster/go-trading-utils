ffc1b0c334f9cd6ae431c86e3343dfbc22f234ee
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-13 17:25:31 +0300

    Refactor channel names in stream functions

--- spot/streams/streams.go
+++ spot/streams/streams.go
@@ -13,44 +13,44 @@ const (
 	ChannelBookTicker = "bookTicker"
 )
 
-func StartUserDataStream(listenKey string, channel chan *binance.WsUserDataEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+func StartUserDataStream(listenKey string, out chan *binance.WsUserDataEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	wsHandler := func(event *binance.WsUserDataEvent) {
-		channel <- event
+		out <- event
 	}
 	return binance.WsUserDataServe(listenKey, wsHandler, handleErr)
 }
 
-func StartDepthStream(symbol string, channel chan *binance.WsDepthEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+func StartDepthStream(symbol string, out chan *binance.WsDepthEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	wsHandler := func(event *binance.WsDepthEvent) {
-		channel <- event
+		out <- event
 	}
 	return binance.WsDepthServe(symbol, wsHandler, handleErr)
 }
 
-func StartKlineStream(symbol string, interval string, channel chan *binance.WsKlineEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+func StartKlineStream(symbol string, interval string, out chan *binance.WsKlineEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	wsHandler := func(event *binance.WsKlineEvent) {
-		channel <- event
+		out <- event
 	}
 	return binance.WsKlineServe(symbol, interval, wsHandler, handleErr)
 }
 
-func StartTradeStream(symbol string, channel chan *binance.WsTradeEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+func StartTradeStream(symbol string, out chan *binance.WsTradeEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	wsHandler := func(event *binance.WsTradeEvent) {
-		channel <- event
+		out <- event
 	}
 	return binance.WsTradeServe(symbol, wsHandler, handleErr)
 }
 
-func StartAggTradeStream(symbol string, channel chan *binance.WsAggTradeEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+func StartAggTradeStream(symbol string, out chan *binance.WsAggTradeEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	wsHandler := func(event *binance.WsAggTradeEvent) {
-		channel <- event
+		out <- event
 	}
 	return binance.WsAggTradeServe(symbol, wsHandler, handleErr)
 }
 
-func StartBookTickerStream(symbol string, channel chan *binance.WsBookTickerEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
+func StartBookTickerStream(symbol string, out chan *binance.WsBookTickerEvent, handleErr binance.ErrHandler) (doneC, stopC chan struct{}, err error) {
 	wsHandler := func(event *binance.WsBookTickerEvent) {
-		channel <- event
+		out <- event
 	}
 	return binance.WsBookTickerServe(symbol, wsHandler, handleErr)
 }
