0199751b04edeffa608273d77810c7e044493cd4
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-03-15 11:41:16 +0300

    Remove unused code and test files

--- spot/info/exchangeinfo.go
+++ spot/info/exchangeinfo.go
@@ -4,37 +4,37 @@ import (
 	"context"
 
 	"github.com/adshao/go-binance/v2"
-	symbol_info "github.com/fr0ster/go-binance-utils/spot/info/symbols"
-	filters_info "github.com/fr0ster/go-binance-utils/spot/info/symbols/filters"
+	symbols_info "github.com/fr0ster/go-binance-utils/spot/info/symbols"
+	symbol_info "github.com/fr0ster/go-binance-utils/spot/info/symbols/symbol"
 )
 
 type ExchangeInfo struct {
 	exchangeInfo *binance.ExchangeInfo
-	Symbols      *symbol_info.Symbols
+	Symbols      *symbols_info.Symbols
 }
 
-func GetExchangeInfo(client *binance.Client) (ExchangeInfo, error) {
+func GetExchangeInfo(client *binance.Client) (*ExchangeInfo, error) {
 	exchangeInfo, err := client.NewExchangeInfoService().Do(context.Background())
 	if err != nil {
-		return ExchangeInfo{}, err
+		return nil, err
 	}
-	symbols := symbol_info.NewSymbols(2, exchangeInfo.Symbols)
-	return ExchangeInfo{exchangeInfo, symbols}, nil
+	symbols := symbols_info.NewSymbols(2, exchangeInfo.Symbols)
+	return &ExchangeInfo{exchangeInfo, symbols}, nil
 }
 
-func (exchangeInfo *ExchangeInfo) GetOrderTypes(symbolname string) []binance.OrderType {
-	res := make([]binance.OrderType, 0)
-	symbol := exchangeInfo.Symbols.GetSymbol(symbolname)
-	for _, orderType := range symbol.OrderTypes {
-		res = append(res, binance.OrderType(orderType))
-	}
-	return res
+func (exchangeInfo *ExchangeInfo) GetSymbol(symbol string) *symbol_info.Symbol {
+	return exchangeInfo.Symbols.GetSymbol(symbol)
 }
 
-func (exchangeInfo *ExchangeInfo) GetPermissions(symbolname string) []string {
-	return exchangeInfo.Symbols.GetSymbol(symbolname).Permissions
-}
+// func (exchangeInfo *ExchangeInfo) GetOrderTypes(symbolname string) []binance.OrderType {
+// 	res := make([]binance.OrderType, 0)
+// 	symbol := exchangeInfo.Symbols.GetSymbol(symbolname)
+// 	for _, orderType := range symbol.OrderTypes {
+// 		res = append(res, binance.OrderType(orderType))
+// 	}
+// 	return res
+// }
 
-func (exchangeInfo *ExchangeInfo) GetFilters(symbolname string) filters_info.Filters {
-	return exchangeInfo.Symbols.GetSymbol(symbolname).Filters
-}
+// func (exchangeInfo *ExchangeInfo) GetPermissions(symbolname string) []string {
+// 	return exchangeInfo.Symbols.GetSymbol(symbolname).Permissions
+// }
--- spot/info/symbols/symbol/symbol.go
+++ spot/info/symbols/symbol/symbol.go
@@ -4,7 +4,6 @@ import (
 	"sync"
 
 	"github.com/adshao/go-binance/v2"
-	filters_info "github.com/fr0ster/go-binance-utils/spot/info/symbols/filters"
 	"github.com/google/btree"
 )
 
@@ -13,7 +12,6 @@ type (
 	Symbol     struct {
 		SymbolName
 		binance.Symbol
-		filters_info.Filters
 		mu sync.Mutex
 	}
 )
@@ -27,12 +25,9 @@ func (s *Symbol) Equal(than btree.Item) bool {
 }
 
 func NewSymbol(degree int, symbol *binance.Symbol) *Symbol {
-	filters := filters_info.NewFilters(degree)
-	filters.Init(convertFilters(symbol.Filters))
 	return &Symbol{
 		SymbolName: SymbolName(symbol.Symbol),
 		Symbol:     *symbol,
-		Filters:    *filters,
 		mu:         sync.Mutex{},
 	}
 }
@@ -47,12 +42,4 @@ func (s *Symbol) TryLock() bool {
 
 func (s *Symbol) Unlock() {
 	s.mu.Unlock()
-}
-
-func convertFilters(filters []map[string]interface{}) []filters_info.Filter {
-	convertedFilters := make([]filters_info.Filter, len(filters))
-	for i, f := range filters {
-		convertedFilters[i] = filters_info.Filter(f)
-	}
-	return convertedFilters
 }
