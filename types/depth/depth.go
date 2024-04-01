86fad628277135fcd945c68b918ea87139fe5a86
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-04-01 20:39:18 +0300

    Fix RestrictAsk and RestrictBid logic

--- types/depth/depth.go
+++ types/depth/depth.go
@@ -139,7 +139,7 @@ func (d *Depth) SetBid(price float64, quantity float64) {
 // RestrictAsk implements depth_interface.Depths.
 func (d *Depth) RestrictAsk(price float64, quantity float64) {
 	d.asks.Ascend(func(i btree.Item) bool {
-		if i.(*DepthItemType).Price >= price {
+		if i.(*DepthItemType).Price < price {
 			d.asks.Delete(i)
 			return false
 		}
@@ -150,7 +150,7 @@ func (d *Depth) RestrictAsk(price float64, quantity float64) {
 // RestrictBid implements depth_interface.Depths.
 func (d *Depth) RestrictBid(price float64, quantity float64) {
 	d.bids.Ascend(func(i btree.Item) bool {
-		if i.(*DepthItemType).Price <= price {
+		if i.(*DepthItemType).Price > price {
 			d.bids.Delete(i)
 			return false
 		}
