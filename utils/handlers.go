f7ec0ab9d4cb7fd272e2281184521ee344373294
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-04-04 19:55:41 +0300

    Refactor shutdown handling in handlers.go

--- utils/handlers.go
+++ utils/handlers.go
@@ -2,17 +2,27 @@ package utils
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func HandleShutdown(stop <-chan os.Signal, delay time.Duration) {
 	select {
 	case <-stop:
-		fmt.Println("Shutting down...")
+		logrus.Info("Shutting down...")
 	case <-time.After(delay):
-		log.Fatal("Shutdown timed out")
+		logrus.Fatal("Shutdown timed out")
+	}
+}
+
+func HandleWaitAndShutdown(stop <-chan os.Signal, delay time.Duration) {
+	select {
+	case <-stop:
+		logrus.Info("Shutting down...")
+	case <-time.After(delay):
+		logrus.Info("Shutdown timed out")
 	}
 }
 
