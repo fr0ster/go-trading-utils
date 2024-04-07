8387d39dd84ed67151da84ae7eee93195ea8ac1c
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-04-07 10:59:38 +0300

    Refactor Spot_depth_buy_sell_signals function in ssbs_signals.go

--- strategy/spot_signals/ssbs_signals.go
+++ strategy/spot_signals/ssbs_signals.go
@@ -23,8 +23,8 @@ func Spot_depth_buy_sell_signals(
 	pair *config_interfaces.Pairs,
 	stopEvent chan os.Signal,
 	triggerEvent chan bool) (buyEvent chan *depth_types.DepthItemType, sellEvent chan *depth_types.DepthItemType) {
-	var boundAsk float64
-	var boundBid float64
+	// var boundAsk float64
+	// var boundBid float64
 	buyEvent = make(chan *depth_types.DepthItemType, 1)
 	sellEvent = make(chan *depth_types.DepthItemType, 1)
 	go func() {
@@ -33,93 +33,32 @@ func Spot_depth_buy_sell_signals(
 			case <-stopEvent:
 				return
 			case <-triggerEvent: // Чекаємо на спрацювання тригера
-				getBaseBalance := func(pair *config_interfaces.Pairs) (
-					baseBalance float64,
-					err error) {
-					baseBalance, err = account.GetAsset((*pair).GetBaseSymbol())
-					return
-				}
-				getTargetBalance := func(pair *config_interfaces.Pairs) (
-					targetBalance float64,
-					err error) {
-					targetBalance, err = account.GetAsset((*pair).GetTargetSymbol())
-					return
-				}
-				baseBalance, err := getBaseBalance(pair)
+				baseBalance,
+					targetBalance,
+					limitBalance,
+					InPositionLimit,
+					ask,
+					bid,
+					boundAsk,
+					boundBid,
+					_, //limitValue,
+					sellQuantity,
+					buyQuantity,
+					err := getData4Analysis(account, depths, pair)
 				if err != nil {
-					logrus.Warnf("Can't get %s balance: %v", (*pair).GetTargetSymbol(), err)
+					logrus.Warnf("Can't get data for analysis: %v", err)
 					continue
 				}
-				targetBalance, err := getTargetBalance(pair)
-				if err != nil {
-					logrus.Warnf("Can't get %s balance: %v", (*pair).GetTargetSymbol(), err)
-
-					continue
-				}
-				limitBalance := (*pair).GetLimitValue()
-
-				getAskAndBid := func(depths *depth_types.Depth) (ask float64, bid float64, err error) {
-					getPrice := func(val btree.Item) float64 {
-						if val == nil {
-							err = errors.New("value is nil")
-						}
-						return val.(*depth_types.DepthItemType).Price
-					}
-					ask = getPrice(depths.GetAsks().Min())
-					bid = getPrice(depths.GetBids().Max())
-					return
-				}
-
-				ask, bid, err := getAskAndBid(depths)
-				if err != nil {
-					logrus.Warnf("Can't get ask and bid: %v", err)
-					continue
-				}
-
-				getBound := func(pair *config_interfaces.Pairs) (boundAsk float64, boundBid float64, err error) {
-					if boundAsk == ask*(1+(*pair).GetBuyDelta()) &&
-						boundBid == bid*(1-(*pair).GetSellDelta()) {
-						err = errors.New("bounds are the same")
-					} else {
-						boundAsk = ask * (1 + (*pair).GetBuyDelta())
-						logrus.Debugf("Ask bound: %f", boundAsk)
-						boundBid = bid * (1 - (*pair).GetSellDelta())
-						logrus.Debugf("Bid bound: %f", boundBid)
-					}
-					return
-				}
-				boundAsk, boundBid, err = getBound(pair)
-				if err != nil {
-					logrus.Warnf("Can't get bounds: %v", err)
-					continue
-				}
-				// Value for BUY and SELL transactions
-				limitValue := (*pair).GetLimitOnTransaction() * limitBalance // Value for one transaction
-
-				// SELL Quantity for one transaction
-				sellQuantity := limitValue / bid // Quantity for one SELL transaction
-				if sellQuantity > targetBalance {
-					sellQuantity = targetBalance // Quantity for one SELL transaction if it's more than available
-				}
-
-				// Correct value for BUY transaction
-				if limitValue > math.Min(limitBalance, baseBalance) {
-					limitValue = math.Min(limitBalance, baseBalance)
-				}
-				// BUY Quantity for one transaction
-				buyQuantity := limitValue / boundAsk
-				// If quantity for one BUY transaction is less than available
-				if buyQuantity*boundAsk < baseBalance &&
-					// And middle price is higher than low bound price
-					((*pair).GetMiddlePrice() == 0 || (*pair).GetMiddlePrice() >= boundAsk) {
+				if buyQuantity*boundAsk < baseBalance && // If quantity for one BUY transaction is less than available
+					((*pair).GetMiddlePrice() == 0 ||
+						targetBalance < InPositionLimit ||
+						(*pair).GetMiddlePrice() >= boundAsk) { // And middle price is higher than low bound price
 					logrus.Infof("Middle price %f is higher than high bound price %f, BUY!!!", (*pair).GetMiddlePrice(), boundAsk)
 					buyEvent <- &depth_types.DepthItemType{
 						Price:    boundAsk,
 						Quantity: buyQuantity}
-					// If quantity for one SELL transaction is less than available
-				} else if sellQuantity <= targetBalance &&
-					// And middle price is lower than low bound price
-					(*pair).GetMiddlePrice() <= boundBid {
+				} else if sellQuantity <= targetBalance && // If quantity for one SELL transaction is less than available
+					(*pair).GetMiddlePrice() <= boundBid { // And middle price is lower than low bound price
 					logrus.Infof("Middle price %f is lower than low bound price %f, SELL!!!", (*pair).GetMiddlePrice(), boundBid)
 					sellEvent <- &depth_types.DepthItemType{
 						Price:    boundBid,
