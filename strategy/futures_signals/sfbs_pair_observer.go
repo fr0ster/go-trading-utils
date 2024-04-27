722a56965fc9db7d32b659479b5d09d812e6e094
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-04-27 23:39:09 +0300

    Refactor delta comparison in PairObserver's StartPriceChangesSignal method to use percentage values and fix bug

--- strategy/futures_signals/sfbs_pair_observer.go
+++ strategy/futures_signals/sfbs_pair_observer.go
@@ -289,7 +289,7 @@ func (pp *PairObserver) StartPriceChangesSignal() chan *pair_price_types.PairDel
 				if priceVal := price.Get(&futures_price.SymbolPrice{Symbol: pp.pair.GetPair()}); priceVal != nil {
 					if utils.ConvStrToFloat64(priceVal.(*futures_price.SymbolPrice).Price) != 0 {
 						delta += (utils.ConvStrToFloat64(priceVal.(*futures_price.SymbolPrice).Price) - last_price) * 100 / last_price
-						if delta > pp.deltaUp*100 || delta < pp.deltaDown*100 {
+						if delta > pp.deltaUp*100 || delta < -pp.deltaDown*100 {
 							pp.priceChanges <- &pair_price_types.PairDelta{
 								Price:   utils.ConvStrToFloat64(priceVal.(*futures_price.SymbolPrice).Price),
 								Percent: utils.RoundToDecimalPlace(delta, 3)}
