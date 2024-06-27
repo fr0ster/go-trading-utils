c8c923084125dba667cf194c8f10374665465719
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-06-27 19:31:20 +0300

    Refactor sfbs_pair_processor.go to optimize notional value calculation and improve code readability

--- strategy/futures_signals/sfbs_pair_processor.go
+++ strategy/futures_signals/sfbs_pair_processor.go
@@ -608,16 +608,13 @@ func (pp *PairProcessor) CalculateInitialPosition(
 	low := pp.roundQuantity(pp.notional / buyPrice)
 	high := pp.roundQuantity(pp.GetFreeBalance() * float64(pp.GetLeverage()) / buyPrice)
 
-	for {
+	for pp.roundQuantity(high-low) > pp.stepSizeDelta {
 		mid := pp.roundQuantity((low + high) / 2)
 		value, n = pp.CalcValueForQuantity(buyPrice, mid, endPrice)
 		if value <= pp.limitOnPosition*float64(pp.leverage) && n >= minN {
 			low = mid
 		} else {
 			high = mid
-		}
-		if high-low <= pp.stepSizeDelta {
-			break
 		}
 	}
 
