11ce2270f78c38cc90dcbc9cf84eaade35ac1d79
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-07-08 22:25:45 +0300

    Refactor Init function to set LastUpdateID in depth object

--- binance/futures/markets/depth/depth.go
+++ binance/futures/markets/depth/depth.go
@@ -26,5 +26,6 @@ func Init(d *depth_types.Depth, client *futures.Client, limit int) (err error) {
 		price, quantity, _ := ask.Parse()
 		d.SetAsk(price, quantity)
 	}
+	d.LastUpdateID = res.LastUpdateID
 	return nil
 }
