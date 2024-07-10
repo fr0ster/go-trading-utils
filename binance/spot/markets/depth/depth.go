e96dd8ab473485ba8cad228ded8f817f443f0228
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-07-10 14:01:56 +0300

    Refactor depth handlers to include LastUpdateID in Init function

--- binance/spot/markets/depth/depth.go
+++ binance/spot/markets/depth/depth.go
@@ -26,5 +26,6 @@ func Init(d *depth_types.Depth, client *binance.Client, limit int) (err error) {
 		price, quantity, _ := ask.Parse()
 		d.SetAsk(price, quantity)
 	}
+	d.LastUpdateID = res.LastUpdateID
 	return nil
 }
