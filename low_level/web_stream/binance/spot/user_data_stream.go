ade8a7b6f2485b30bd0ec4a920e5989093496bce
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-08-05 22:53:26 +0300

    Refactor JSON parsing functions for OrderTradeUpdate struct in binance spot and futures user data streams

--- low_level/web_stream/binance/spot/user_data_stream.go
+++ low_level/web_stream/binance/spot/user_data_stream.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/bitly/go-simplejson"
 	api_common "github.com/fr0ster/go-trading-utils/low_level/common"
 	spot_rest "github.com/fr0ster/go-trading-utils/low_level/rest_api/binance/spot"
 	api "github.com/fr0ster/go-trading-utils/low_level/rest_api/common"
@@ -14,139 +15,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// // Визначення структури для JSON OrderTradeUpdate
-// // WsUserDataEvent define user data event
-// type WsUserDataEvent struct {
-// 	Event         types.UserDataEventType `json:"e"`
-// 	Time          int64                   `json:"E"`
-// 	AccountUpdate WsAccountUpdateList
-// 	BalanceUpdate WsBalanceUpdate
-// 	OrderUpdate   WsOrderUpdate
-// 	OCOUpdate     WsOCOUpdate
-// }
-
-// type WsAccountUpdateList struct {
-// 	AccountUpdateTime int64             `json:"u"`
-// 	WsAccountUpdates  []WsAccountUpdate `json:"B"`
-// }
-
-// // WsAccountUpdate define account update
-// type WsAccountUpdate struct {
-// 	Asset  string `json:"a"`
-// 	Free   string `json:"f"`
-// 	Locked string `json:"l"`
-// }
-
-// type WsBalanceUpdate struct {
-// 	Asset           string `json:"a"`
-// 	Change          string `json:"d"`
-// 	TransactionTime int64  `json:"T"`
-// }
-
-// type WsOrderUpdate struct {
-// 	Symbol                  string                `json:"s"`
-// 	ClientOrderId           string                `json:"c"`
-// 	Side                    string                `json:"S"`
-// 	Type                    string                `json:"o"`
-// 	TimeInForce             types.TimeInForceType `json:"f"`
-// 	Volume                  string                `json:"q"`
-// 	Price                   string                `json:"p"`
-// 	StopPrice               string                `json:"P"`
-// 	IceBergVolume           string                `json:"F"`
-// 	OrderListId             int64                 `json:"g"` // for OCO
-// 	OrigCustomOrderId       string                `json:"C"` // customized order ID for the original order
-// 	ExecutionType           string                `json:"x"` // execution type for this event NEW/TRADE...
-// 	Status                  string                `json:"X"` // order status
-// 	RejectReason            string                `json:"r"`
-// 	Id                      int64                 `json:"i"` // order id
-// 	LatestVolume            string                `json:"l"` // quantity for the latest trade
-// 	FilledVolume            string                `json:"z"`
-// 	LatestPrice             string                `json:"L"` // price for the latest trade
-// 	FeeAsset                string                `json:"N"`
-// 	FeeCost                 string                `json:"n"`
-// 	TransactionTime         int64                 `json:"T"`
-// 	TradeId                 int64                 `json:"t"`
-// 	IgnoreI                 int64                 `json:"I"` // ignore
-// 	IsInOrderBook           bool                  `json:"w"` // is the order in the order book?
-// 	IsMaker                 bool                  `json:"m"` // is this order maker?
-// 	IgnoreM                 bool                  `json:"M"` // ignore
-// 	CreateTime              int64                 `json:"O"`
-// 	FilledQuoteVolume       string                `json:"Z"` // the quote volume that already filled
-// 	LatestQuoteVolume       string                `json:"Y"` // the quote volume for the latest trade
-// 	QuoteVolume             string                `json:"Q"`
-// 	SelfTradePreventionMode string                `json:"V"`
-
-// 	//These are fields that appear in the payload only if certain conditions are met.
-// 	TrailingDelta              int64  `json:"d"` // Appears only for trailing stop orders.
-// 	TrailingTime               int64  `json:"D"`
-// 	StrategyId                 int64  `json:"j"` // Appears only if the strategyId parameter was provided upon order placement.
-// 	StrategyType               int64  `json:"J"` // Appears only if the strategyType parameter was provided upon order placement.
-// 	PreventedMatchId           int64  `json:"v"` // Appears only for orders that expired due to STP.
-// 	PreventedQuantity          string `json:"A"`
-// 	LastPreventedQuantity      string `json:"B"`
-// 	TradeGroupId               int64  `json:"u"`
-// 	CounterOrderId             int64  `json:"U"`
-// 	CounterSymbol              string `json:"Cs"`
-// 	PreventedExecutionQuantity string `json:"pl"`
-// 	PreventedExecutionPrice    string `json:"pL"`
-// 	PreventedExecutionQuoteQty string `json:"pY"`
-// 	WorkingTime                int64  `json:"W"` // Appears when the order is working on the book
-// 	MatchType                  string `json:"b"`
-// 	AllocationId               int64  `json:"a"`
-// 	WorkingFloor               string `json:"k"`  // Appears for orders that could potentially have allocations
-// 	UsedSor                    bool   `json:"uS"` // Appears for orders that used SOR
-// }
-
-// type WsOCOUpdate struct {
-// 	Symbol          string `json:"s"`
-// 	OrderListId     int64  `json:"g"`
-// 	ContingencyType string `json:"c"`
-// 	ListStatusType  string `json:"l"`
-// 	ListOrderStatus string `json:"L"`
-// 	RejectReason    string `json:"r"`
-// 	ClientOrderId   string `json:"C"` // List Client Order ID
-// 	TransactionTime int64  `json:"T"`
-// 	Orders          WsOCOOrderList
-// }
-
-// type WsOCOOrderList struct {
-// 	WsOCOOrders []WsOCOOrder `json:"O"`
-// }
-
-// type WsOCOOrder struct {
-// 	Symbol        string `json:"s"`
-// 	OrderId       int64  `json:"i"`
-// 	ClientOrderId string `json:"c"`
-// }
-
-// // Функція для парсингу JSON
-// func parseJSON(data []byte) (*WsUserDataEvent, error) {
-// 	var orderTradeUpdate WsUserDataEvent
-// 	err := json.Unmarshal(data, &orderTradeUpdate)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &orderTradeUpdate, nil
-// }
-
-type GeneralResponse struct {
-	ExecutionReport string `json:"e"`
-}
-
-// Функція для парсингу JSON
-func parseResponse(data []byte) *GeneralResponse {
-	var generalResponse GeneralResponse
-	_ = json.Unmarshal(data, &generalResponse)
-	return &generalResponse
-}
-
 type UserDataStream struct {
-	apiKey  string
-	baseUrl string
-	// accountUpdateCallBack func(*WsAccountUpdateList)
-	// balanceUpdateCallBack func(*WsBalanceUpdate)
-	// orderUpdateCallBack   func(*WsOrderUpdate)
-	// ocoUpdateCallBack     func(*WsOCOUpdate)
+	apiKey string
 }
 
 func (uds *UserDataStream) listenKey(method string, useTestNet ...bool) (listenKey string, err error) {
@@ -165,7 +35,7 @@ func (uds *UserDataStream) listenKey(method string, useTestNet ...bool) (listenK
 	return
 }
 
-func (uds *UserDataStream) Start(callBack func(*GeneralResponse), quit chan struct{}, useTestNet ...bool) {
+func (uds *UserDataStream) Start(callBack func(*simplejson.Json), quit chan struct{}, useTestNet ...bool) {
 	wss := GetWsBaseUrl(useTestNet...)
 	listenKey, err := uds.listenKey(http.MethodPost, useTestNet...)
 	if err != nil {
@@ -177,18 +47,9 @@ func (uds *UserDataStream) Start(callBack func(*GeneralResponse), quit chan stru
 		if err != nil {
 			logrus.Fatalf("Error parsing JSON: %v, message: %s", err, message)
 		}
-		logrus.Debugf("Message: %s", json.MustString())
-		// switch json.Get("e").MustString() {
-		// case "ACCOUNT_UPDATE":
-		// 	var accountUpdateList WsAccountUpdateList
-		// 	err := json.Unmarshal(&accountUpdateList)
-		// 	if err != nil {
-		// 		logrus.Fatalf("Error parsing ACCOUNT_UPDATE: %v, message: %s", err, message)
-		// 	}
-		// 	if uds.accountUpdateCallBack != nil {
-		// 		uds.accountUpdateCallBack(&accountUpdateList)
-		// 	}
-		// }
+		if callBack != nil {
+			callBack(json)
+		}
 	}, quit)
 	go func() {
 		for {
