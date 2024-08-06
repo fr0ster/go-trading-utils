6ec60ae50c2eca7985748073eccaaa2c2ccaa098
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-08-06 16:51:23 +0300

    Refactor web_api package to simplify code and improve readability

---
+++ low_level/web_api/binance/logout.go
@@ -0,0 +1,19 @@
+package spot_web_api
+
+import (
+	"encoding/json"
+
+	web_api "github.com/fr0ster/go-trading-utils/low_level/web_api/common"
+)
+
+// Функція для логіну
+func (wa *WebApi) Logout() (response *LogonResponse, limit []web_api.RateLimit, err error) {
+	method := "session.logout"
+
+	body, limit, err := web_api.CallWebAPI(wa.waHost, wa.waPath, method, nil)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &response)
+	return
+}
