72c1c38922075ced9cd3e5204dafceb871903ea1
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-08-07 12:54:45 +0300

    Refactor package structure for low_level/common and low_level/utils

--- binance/futures/exchangeinfo/exchangeinfo.go
+++ binance/futures/exchangeinfo/exchangeinfo.go
@@ -12,51 +12,6 @@ import (
 	"github.com/fr0ster/go-trading-utils/utils"
 )
 
-// func InitCreator(degree int, client *futures.Client) func(*exchange_types.ExchangeInfo) types.InitFunction {
-// 	return func(val *exchange_types.ExchangeInfo) types.InitFunction {
-// 		return func() (err error) {
-// 			var (
-// 				exchangeInfo *futures.ExchangeInfo
-// 			)
-// 			exchangeInfo, err = client.NewExchangeInfoService().Do(context.Background())
-// 			if err != nil {
-// 				return
-// 			}
-// 			val.Timezone = exchangeInfo.Timezone
-// 			val.ServerTime = exchangeInfo.ServerTime
-// 			val.RateLimits = ConvertRateLimits(exchangeInfo.RateLimits)
-// 			val.ExchangeFilters = exchangeInfo.ExchangeFilters
-// 			val.Symbols, err = symbols_types.New(
-// 				degree,
-// 				func() (symbols []*symbol_types.SymbolInfo) {
-// 					for _, s := range exchangeInfo.Symbols {
-// 						orderTypes := make([]symbol_types.OrderType, len(s.OrderType))
-// 						for i, ot := range s.OrderType {
-// 							orderTypes[i] = symbol_types.OrderType(ot)
-// 						}
-// 						symbols = append(symbols, symbol_types.New(
-// 							s.Symbol,
-// 							items_types.ValueType(utils.ConvStrToFloat64(s.MinNotionalFilter().Notional)),
-// 							items_types.QuantityType(utils.ConvStrToFloat64(s.LotSizeFilter().StepSize)),
-// 							items_types.QuantityType(utils.ConvStrToFloat64(s.LotSizeFilter().MaxQuantity)),
-// 							items_types.QuantityType(utils.ConvStrToFloat64(s.LotSizeFilter().MinQuantity)),
-// 							items_types.PriceType(utils.ConvStrToFloat64(s.PriceFilter().TickSize)),
-// 							items_types.PriceType(utils.ConvStrToFloat64(s.PriceFilter().MaxPrice)),
-// 							items_types.PriceType(utils.ConvStrToFloat64(s.PriceFilter().MinPrice)),
-// 							symbol_types.QuoteAsset(s.QuoteAsset),
-// 							symbol_types.BaseAsset(s.BaseAsset),
-// 							false,
-// 							nil,
-// 							orderTypes,
-// 						))
-// 					}
-// 					return
-// 				})
-// 			return
-// 		}
-// 	}
-// }
-
 func InitCreator(client *futures.Client, degree int, symbols ...string) func(val *exchange_types.ExchangeInfo) types.InitFunction {
 	return func(val *exchange_types.ExchangeInfo) types.InitFunction {
 		return func() (err error) {
