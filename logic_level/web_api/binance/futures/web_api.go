e2c075037197d3f15d7aaca3c9851079d937efe4
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-08-09 12:53:33 +0300

    chore: Update dependencies and refactor package structure for low_level/common and low_level/utils

--- logic_level/web_api/binance/futures/web_api.go
+++ logic_level/web_api/binance/futures/web_api.go
@@ -2,11 +2,21 @@ package futures_web_api
 
 import (
 	common "github.com/fr0ster/go-trading-utils/logic_level/web_api/binance/common"
+	"github.com/fr0ster/go-trading-utils/logic_level/web_api/binance/common/order"
 	signature "github.com/fr0ster/turbo-restler/utils/signature"
 	"github.com/fr0ster/turbo-restler/web_api"
 )
 
-func New(apiKey, apiSecret, symbol string, sign signature.Sign, useTestNet ...bool) *common.WebApi {
+type WebApi interface {
+	PlaceOrder() *order.Order
+	CancelOrder() *order.Order
+	QueryOrder() *order.Order
+	CancelReplaceOrder() *order.Order
+	QueryOpenOrders() *order.Order
+	QueryAllOrders() *order.Order
+}
+
+func New(apiKey, apiSecret, symbol string, sign signature.Sign, useTestNet ...bool) WebApi {
 	var (
 		waHost string
 		waPath string
